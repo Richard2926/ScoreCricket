054dcb3a10dfc5931141bbbe6af0764a55e86282
Richard2926 <richardr2926@Gmail.com>
2019-08-03 17:33:28 -0400

    Add files via upload

---
+++ ScoreCricket/Scoreboard/Scoreboard/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Scoreboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Player1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(704, 76);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome To Smart Cricket";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 171);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start New Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(463, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 174);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add / Remove Players";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(783, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 173);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "View Stats";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.Player1);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1264, 985);
+            this.panel1.TabIndex = 4;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(461, 36);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 56);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "View";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Player1
+            // 
+            this.Player1.AutoSize = true;
+            this.Player1.Location = new System.Drawing.Point(156, 153);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(0, 13);
+            this.Player1.TabIndex = 3;
+            this.Player1.Click += new System.EventHandler(this.Player1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(111, 701);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 49);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Back";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 985);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label Player1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ScoreCricket/Scoreboard/Scoreboard/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Scoreboard
+{
+    public partial class Form1 : Form
+    {
+        String sb1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+
+             String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("F:\\ScoreCricket\\Stats.txt");
+                StringBuilder sb = new StringBuilder("", 50);
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    char val = (char) sr.Read();
+                    if (val == ' ')
+                    {
+                        Console.WriteLine(sb);
+                        Player1.Text = sb.ToString();
+                        sb1 = sb.ToString();
+                        sb.Clear();
+                    } else
+                    {
+                        sb.Append(new char[] { val });
+                    }
+                    //write the lie to console window
+                    //Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+
+                    sb.Append(new char[] { 'D', 'E', 'F' });
+                }
+
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine("Exception: " + f.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            } 
+        }
+
+        
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void Player1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Player1.Text = sb1;
+        }
+    }
+}
